9fdafffbe1d8aca18c2df7151d1319c544e9f595
Nick Sarten <nick.sarten@narrative.so>
2020-04-13 23:21:49 +1200

    Initial working static build
    This is a series of hacks to get static building working, but it needs
    to be cleaned up and refactored into something that will work across
    more than one or two platforms.

--- exempi-sys/build.rs
+++ exempi-sys/build.rs
@@ -1,11 +1,28 @@
 extern crate pkg_config;
+use std::env;
 
 fn main() {
-    match pkg_config::find_library("exempi-2.0 >= 2.4.0") {
+    match pkg_config::Config::new().atleast_version("2.4.0").statik(true).probe("exempi-2.0") {
         Ok(_) => (),
         Err(e) => {
-            println!("Exempi not found");
+            println!("cargo:warning=Exempi not found");
             panic!(e);
         }
+    }
+    println!("cargo:rustc-link-lib=expat");
+    match pkg_config::Config::new().statik(true).probe("zlib") {
+        Ok(_) => (),
+        Err(e) => {
+            println!("cargo:warning=zlib not found");
+            panic!(e);
+        }
+    }
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "macos" {
+        println!("cargo:rustc-link-lib=c++");
+        println!("cargo:rustc-link-lib=framework=CoreFoundation");
+        println!("cargo:rustc-link-lib=framework=CoreServices");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
     }
 }
