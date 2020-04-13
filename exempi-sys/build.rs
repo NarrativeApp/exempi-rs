9ab1c169afc31a03dc6f512c896beed7441028cf
Nick Sarten <nick.sarten@narrative.so>
2020-04-14 11:01:53 +1200

    Added static libz and expat build script
    The build now pulls in these libraries from respective sys crates so we
    don't have to worry about dependency installation at the system level

--- exempi-sys/build.rs
+++ exempi-sys/build.rs
@@ -1,5 +1,6 @@
 extern crate pkg_config;
 use std::env;
+use std::path::Path;
 
 fn main() {
     match pkg_config::Config::new().atleast_version("2.4.0").statik(true).probe("exempi-2.0") {
@@ -9,8 +10,15 @@ fn main() {
             panic!(e);
         }
     }
-    println!("cargo:rustc-link-lib=expat");
-    println!("cargo:rustc-link-lib=zlib");
+
+    let libz_root = env::var("DEP_Z_ROOT").expect("Couldn't find zlib build root");
+    let expat_root = env::var("DEP_EXPAT_ROOT").expect("Couldn't find expat build root");
+
+    println!("cargo:rustc-link-search={:?}", Path::new(&libz_root).join("build"));
+    println!("cargo:rustc-link-search={:?}", Path::new(&expat_root).join("lib"));
+    println!("cargo:rustc-link-lib=static=expat");
+    println!("cargo:rustc-link-lib=static=z");
+
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     if target_os == "macos" {
         println!("cargo:rustc-link-lib=c++");
