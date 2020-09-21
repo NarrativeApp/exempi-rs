c279f15953428dd010410b15b53f0cd3a1ac33a3
Nick Sarten <nick.sarten@narrative.so>
2020-09-21 23:18:04 +1200

    Cleaned up redundant code and stopped constant rebuilds.

--- exempi-sys/build.rs
+++ exempi-sys/build.rs
@@ -3,23 +3,8 @@ extern crate pkg_config;
 #[cfg(feature = "static")]
 fn link_static() {
     use std::env;
-    use std::path::Path;
-
-    let libz_root = env::var("DEP_Z_ROOT")
-        .expect("Couldn't find zlib build root, assuming there's a built-in installation of zlib");
-    println!(
-        "cargo:rustc-link-search={:?}",
-        Path::new(&libz_root).join("build")
-    );
-    let expat_root = env::var("DEP_EXPAT_OUTDIR")
-        .expect("Couldn't find expat build root, assuming there's a built-in installation of expat");
-    println!(
-        "cargo:rustc-link-search={:?}",
-        Path::new(&expat_root).join("lib")
-    );
-
-    // println!("cargo:rustc-link-lib=static=expat");
-    // println!("cargo:rustc-link-lib=static=z");
+    println!("cargo:rustc-link-lib=dylib=expat");
+    println!("cargo:rustc-link-lib=dylib=z");
 
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     if target_os == "macos" {
@@ -30,31 +15,12 @@ fn link_static() {
 
 #[cfg(feature = "static")]
 fn build_exempi() {
-    use std::env;
-    use std::path::Path;
     println!("Building exempi from source...");
-
-    // TODO: build exempi
-    // let libz_root = env::var("DEP_Z_ROOT")
-    //     .expect("Couldn't find zlib build root");
-    // let libz_lib = Path::new(&libz_root).join("build").to_str().unwrap().to_owned();
-    // let libz_inc = Path::new(&libz_root).join("include").to_str().unwrap().to_owned();
-    // let expat_root = env::var("DEP_EXPAT_OUTDIR")
-    //     .expect("Couldn't find expat build root");
-    // let expat_lib = Path::new(&expat_root).join("lib").to_str().unwrap().to_owned();
-    // let expat_inc = Path::new(&expat_root).join("include").to_str().unwrap().to_owned();
 
     let dst = autotools::Config::new("src/exempi")
         .reconf("-ivf")
-        // .cflag(format!("-L{} -I{}", libz_lib, libz_inc))
-        // .cflag(format!("-L{} -I{}", expat_lib, expat_inc))
-        // .cxxflag(format!("-L{} -I{}", libz_lib, libz_inc))
-        // .cxxflag(format!("-L{} -I{}", expat_lib, expat_inc))
-        // .ldflag(format!("-L{}", libz_lib))
-        // .ldflag(format!("-L{}", expat_lib))
         .build();
 
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib").display()
@@ -63,19 +29,18 @@ fn build_exempi() {
     println!("cargo:include={}", dst.join("include").display());
     println!("cargo:lib={}", dst.join("lib").display());
     println!("cargo:root={}", dst.display());
-    // if target_os == "macos" {
-    //     println!("cargo:rustc-link-lib=c++");
-    // } else {
-    //     println!("cargo:rustc-link-lib=stdc++");
-    // }
+
+    // rebuild this library if any key files change (TODO: include more files)
+    println!("cargo:rerun-if-changed=src/exempi/exempi/xmp.h");
 }
 
 fn main() {
     if cfg!(feature = "static") {
+        // build exempi from source and link statically
         #[cfg(feature = "static")]
         {
             build_exempi();
-            //link_static();
+            link_static();
         }
     } else {
         // attempt to link exempi dynamically
